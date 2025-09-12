ae2eab484be4413393f90f0d30cfa5b42bad05fd
Divan <1254256257@qq.com>
2025-09-12 21:44:34 +0800

    自定义图标不再默认启用

--- src/main/java/cn/boop/necron/config/impl/TitleOptionsImpl.java
+++ src/main/java/cn/boop/necron/config/impl/TitleOptionsImpl.java
@@ -13,7 +13,7 @@ public class TitleOptionsImpl extends ModConfig {
     @Switch(name = "Change Title", description = "Use custom window title")
     public static boolean title = true;
     @Switch(name = "Change Icon", description = "Use Custom window icon")
-    public static boolean icon = true;
+    public static boolean icon = false;
     @Switch(name = "Use your title text", size = 2)
     public static boolean urTitle = false;
     @Text(name = "Title Text", placeholder = "Text here")
